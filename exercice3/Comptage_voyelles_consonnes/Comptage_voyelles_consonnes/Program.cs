a5047cf92ee8d2ce591107df807fdba187c9096f
lmilbor <lmilbor@LMILBOR17-DE>
2017-03-23 13:36:23 +0100

    exercice sur ref et out avec les voyelle et les consonnes

---
+++ exercice3/Comptage_voyelles_consonnes/Comptage_voyelles_consonnes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comptage_voyelles_consonnes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mot = "Livre";
+            int nbVoyelle, nbConsonne;
+            NombreVoyellesConsonnes(mot, out nbVoyelle, out nbConsonne);
+            Console.WriteLine("\"{0}\" comprend {1} consonnes et {2} voyelles ", mot, nbConsonne, nbVoyelle);
+            Console.ReadKey();
+        }
+        static bool EstUneVoyelle(char lettre)
+        {
+            bool estUneVoyelle = false;
+            string voyelles = "AEIOUYaeiouy";
+            int compteur = 0;
+            while (compteur < voyelles.Length && !estUneVoyelle)
+            {
+                if (lettre == voyelles[compteur])
+                    estUneVoyelle = true;
+                compteur++;
+            }
+            return estUneVoyelle;
+        }
+
+        static void NombreVoyellesConsonnes(string mot, out int nbVoyelle, out int nbConsonne)
+        {
+            nbVoyelle = 0;
+            mot = mot.ToUpper();
+            for (int i=0; i<mot.Length; i++)
+            {
+                if (EstUneVoyelle(mot[i]))
+                    nbVoyelle++;
+            }
+            nbConsonne = mot.Length - nbVoyelle;
+        }
+    }
+}
