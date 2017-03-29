425465dac6ec98a0f9db43264621bd02aa4c5bd7
lmilbor <lmilbor@LMILBOR17-DE>
2017-03-29 12:13:05 +0200

    Ajout de propriété et méthodes abstraites.

--- exercice6/Vehicules/Vehicules/Program.cs
+++ exercice6/Vehicules/Vehicules/Program.cs
@@ -13,7 +13,9 @@ namespace Vehicules
             Vehicule r5 = new Voiture("zef", energies.Essence);
             Vehicule honda = new Moto("CBF600", energies.Essence);
             Console.WriteLine(r5.Description);
+            Console.WriteLine("La {0} coute " + r5.PRK() + " euros par kilomètres.", r5.Nom);
             Console.WriteLine(honda.Description);
+            Console.WriteLine("La {0} coute " + honda.PRK() + " euros par kilomètres.", honda.Nom);
             Console.ReadKey();
         }
     }
--- exercice6/Vehicules/Vehicules/Vehicule.cs
+++ exercice6/Vehicules/Vehicules/Vehicule.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vehicules
 {
-    public class Vehicule
+    public abstract class Vehicule
     {
         #region Propriétés
 
@@ -18,6 +18,8 @@ namespace Vehicules
             get { return string.Format("Véhicule {0} roule sur {1} roues et à l'énergie {2}", Nom, NbRoues, Energie) ; }
         }
 
+        public abstract void CalculerConso();
+        public abstract double PRK { get; }
         #endregion
         public Vehicule(string nom, int nbRoues, energies energie)
         {
@@ -26,14 +28,23 @@ namespace Vehicules
             Energie = energie;
         }
     }
-
     public class Voiture : Vehicule
     {
         public override string Description
         {
             get { return string.Format("Je suis une voiture \r\n") + base.Description ; }
         }
-        public Voiture(string nom, energies energie) : base(nom, 4, energie) { }
+       public Voiture(string nom, energies energie) : base(nom, 4, energie) { }
+
+        public override void CalculerConso()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double PRK
+        {
+           get { return 0.40; }
+        }
     }
 
     public class Moto : Vehicule
@@ -45,6 +56,16 @@ namespace Vehicules
         public Moto(string nom, energies energie) :base(nom, 2, energie)
         {
 
+        }
+
+        public override void CalculerConso()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double PRK
+        {
+            get { return 0.35; }
         }
     }
 }
