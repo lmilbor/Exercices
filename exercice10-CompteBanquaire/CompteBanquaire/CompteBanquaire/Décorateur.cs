cab07e969fd9aacaabeb55f464c5cd0e456e0520
lmilbor <lmilbor@LMILBOR17-DE>
2017-04-05 17:18:07 +0200

    début exercice decorateur

---
+++ exercice10-CompteBanquaire/CompteBanquaire/CompteBanquaire/Décorateur.cs
@@ -0,0 +1,33 @@
+﻿using POO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompteBanquaire
+{
+    public abstract class Décorateur : MoyenPaiement
+    {
+        protected MoyenPaiement moyenPaiement;
+
+        // Constructeur
+        public Décorateur(MoyenPaiement moyenPaiementIn)
+        {
+            moyenPaiement = moyenPaiementIn;
+        }
+    }
+
+    public class Prevenable : Décorateur
+    {
+        public Prevenable()
+        {
+
+        }
+
+        public override string Payer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
