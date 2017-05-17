65e1c732e4b04cf34aee32f752e7dbf1f792cb2d
lmilbor <lmilbor@LMILBOR17-DE>
2017-05-17 10:01:39 +0200

    Début exercice Saise de Tâches

---
+++ WPF/ExerciceWPF/SaisieTaches/AccesDonnees.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SaisieTaches
+{
+    static public class AccesDonnees
+    {
+        static public List<Tache> ChargerTaches()
+        {
+            var taches = new List<Tache>();
+            XmlSerializer deserializer = new XmlSerializer(typeof(List<Tache>),
+      new XmlRootAttribute("Taches"));
+
+            using (var sr = new StreamReader(@"..\..\Taches.xml"))
+            {
+                // Deserialize renvoie un type object, qu'il faut transtyper 
+                taches = (List<Tache>)deserializer.Deserialize(sr);
+            }
+
+            return taches;
+        }
+    }
+}
---
+++ WPF/ExerciceWPF/SaisieTaches/Contexte.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SaisieTaches
+{
+    public class Contexte
+    {
+        public List<Tache> Taches { get; private set; }
+        public Contexte()
+        {
+            Taches = AccesDonnees.ChargerTaches();
+        }
+
+        // Commande d'ajout
+        private ICommand _cmdAjouter;
+        public ICommand CmdAjouter
+        {
+            get
+            {
+                if (_cmdAjouter == null)
+                    _cmdAjouter = new RelayCommand(AjouterTache);
+
+                return _cmdAjouter;
+            }
+        }
+
+        private ICommand _cmdSupprimer;
+        public ICommand CmdSupprimer
+        {
+            get
+            {
+                if (_cmdSupprimer == null)
+                    _cmdSupprimer = new RelayCommand(SupprimerTache);
+
+                return _cmdSupprimer;
+            }
+        }
+
+        private ICommand _cmdEnregistrer;
+        public ICommand CmdEnregistrer
+        {
+            get
+            {
+                if (_cmdEnregistrer == null)
+                    _cmdEnregistrer = new RelayCommand(EnregistrerTache);
+
+                return _cmdEnregistrer;
+            }
+        }
+
+        private void EnregistrerTache(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SupprimerTache(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AjouterTache(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
